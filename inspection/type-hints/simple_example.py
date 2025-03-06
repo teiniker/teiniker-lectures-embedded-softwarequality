dfcec6f3aec86461f4c28e18da7cbb7e2b234e3a
teiniker <egon.teiniker@gmail.com>
2025-03-06 12:21:22 +0100

    Added examples.

--- inspection/type-hints/simple_example.py
+++ inspection/type-hints/simple_example.py
@@ -1,15 +1,11 @@
 
-data_list :list[str] = []
-
+data_list:list[str] = []
 data_list.append('one')
 
 data_tuple: tuple[str, ...] = ("1", "2")
-
 data_dict: dict[int, str] = {}
-
 
 def add(value_a:int, value_b:int) -> int:
     return value_a + value_b
-
 
 assert 2+3 == add(2, 3)
---
+++ inspection/type-hints/user_table/user_table_exercise.py
@@ -0,0 +1,49 @@
+
+class User():
+    def __init__(self, oid, username, password):
+        self.oid = oid
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return f"User: id={self.oid}, username='{self.username}'"
+
+    def __eq__(self, other):
+        return self.oid == other.oid and self.username == other.username
+
+
+class UserTable():
+    def __init__(self):
+        self._users = []    # ---[*]-> [User]
+
+    def insert(self, user):
+        self._users.append(user)
+
+    def find_by_id(self, oid):
+        for user in self._users:
+            if user.oid == oid:
+                return user
+        return None
+
+    def find_all(self):
+        return self._users
+
+
+if __name__ == '__main__':
+
+    # Verify implementations
+    bart1 = User(1, 'bart', 'EatMyShorts!')
+    bart2 = User(1, 'bart', '#Kig%/5gT54$§')
+    assert bart1 == bart2
+    assert "User: id=1, username='bart'" == str(bart1)
+
+    homer = User(3, 'homer', 'MoreDuff4Me!')
+    marge = User(7, 'marge', 'Kh7gT.9/8#gH')
+
+    table = UserTable()
+    table.insert(homer)
+    table.insert(marge)
+
+    assert 'marge' == table.find_by_id(7).username
+    assert 'homer' == table.find_by_id(3).username
+    assert 2 == len(table.find_all())
