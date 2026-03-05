194240260f406495b6ff68a9b7ae7c6e81ae20e6
teiniker <egon.teiniker@gmail.com>
2026-03-05 17:41:04 +0100

    Moved examples to lectures-python repo.

--- testing/unit-tests/basics/comparator-test/comparator_test_exercise.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-def compare(value_a:int, value_b:int) -> int:
-    if value_a > value_b:
-        return 1
-    elif value_a < value_b:
-        return -1
-    else:
-        return 0
--- testing/unit-tests/basics/product-test/product.py
+++
@@ -1,9 +0,0 @@
-class Product:
-    def __init__(self, description:str, price:int) -> None:
-        if len(description) == 0:
-            raise ValueError(f"Invalid description: '{description}'!'")
-        if price < 0:
-            raise ValueError(f"Invalid price: {price}!")
-
-        self.description = description
-        self.price = price
