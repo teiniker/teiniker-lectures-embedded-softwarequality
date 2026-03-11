8752d3d2335cf23946debccb33ef49b4879d9135
teiniker <egon.teiniker@gmail.com>
2026-03-11 10:49:18 +0100

    Moved simple tests to the lectures-python repo.

--- testing/unit-tests/basics/time-test/time_test_exercise.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from date_time import Time
-
-class TimeTest(unittest.TestCase):
-    # TODO: Implement test cases here ...
-    pass
-
-if __name__ == '__main__':
-    unittest.main()
