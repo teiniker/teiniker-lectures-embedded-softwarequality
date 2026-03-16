23ce4829ef7c248cabf91a63d82521c0b74fcc12
teiniker <egon.teiniker@gmail.com>
2026-03-16 13:51:34 +0100

    Refactoring.

--- testing/unit-tests/doubles/data_analysis_patch/test_data_analysis_patch.py
+++ testing/unit-tests/doubles/data_analysis_patch/test_data_analysis_patch.py
@@ -6,7 +6,8 @@ def test_mean_value_mocked(mocker):
     # Setup
     dao = DataAccessObject("data.csv")
     service = DataAnalysisService(dao)
-    mocker.patch("data_analysis.DataAccessObject.read_data", return_value=[1.0, 2.0, 3.0, 4.0, 5.0])
+    mock_read_data = mocker.patch("data_analysis.DataAccessObject.read_data")
+    mock_read_data.return_value = [1.0, 2.0, 3.0, 4.0, 5.0]
     # Exercise
     mean = service.mean_value()
     # Verify
@@ -17,7 +18,8 @@ def test_max_value_mocked(mocker):
     # Setup
     dao = DataAccessObject("data.csv")
     service = DataAnalysisService(dao)
-    mocker.patch("data_analysis.DataAccessObject.read_data", return_value=[1.0, 2.0, 3.0, 4.0, 5.0])
+    mock_read_data = mocker.patch("data_analysis.DataAccessObject.read_data")
+    mock_read_data.return_value = [1.0, 2.0, 3.0, 4.0, 5.0]
     # Exercise
     max_value = service.max_value()
     # Verify
@@ -28,7 +30,8 @@ def test_mean_value_exception(mocker):
     # Setup
     dao = DataAccessObject("data.csv")
     service = DataAnalysisService(dao)
-    mocker.patch("data_analysis.DataAccessObject.read_data", side_effect=DataAccessError())
+    mock_read_data = mocker.patch("data_analysis.DataAccessObject.read_data")
+    mock_read_data.side_effect = DataAccessError()
     # Exercise / Verify
     with pytest.raises(ServiceError):
         service.mean_value()
@@ -38,7 +41,8 @@ def test_max_value_exception(mocker):
     # Setup
     dao = DataAccessObject("data.csv")
     service = DataAnalysisService(dao)
-    mocker.patch("data_analysis.DataAccessObject.read_data", side_effect=DataAccessError())
+    mock_read_data = mocker.patch("data_analysis.DataAccessObject.read_data")
+    mock_read_data.side_effect = DataAccessError()
     # Exercise / Verify
     with pytest.raises(ServiceError):
         service.max_value()
