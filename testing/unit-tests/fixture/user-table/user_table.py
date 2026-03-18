37ceaf931f827b26fa0ddb7f6e6748085a4ba555
teiniker <egon.teiniker@gmail.com>
2026-03-18 14:16:30 +0100

    Added example.

---
+++ testing/unit-tests/fixture/user-table/user_table.py
@@ -0,0 +1,59 @@
+
+class User():
+    oid: int
+    username: str
+    password: str
+
+    def __init__(self, oid: int, username: str, password: str) -> None:
+        self.oid = oid
+        self.username = username
+        self.password = password
+
+    def __str__(self) -> str:
+        return f"User: id={self.oid}, username='{self.username}'"
+
+    def __eq__(self, other) -> bool:
+        return self.oid == other.oid and self.username == other.username
+
+
+class UserTable():
+    users: list[User]
+
+    def __init__(self) -> None:
+        self.users = []    # ---[*]-> [User]
+
+    def insert(self, user: User) -> None:
+        self.users.append(user)
+
+    def find_by_id(self, oid: int) -> User | None:
+        for user in self.users:
+            if user.oid == oid:
+                return user
+        return None
+
+    def find_all(self) -> list[User]:
+        return self.users
+
+if __name__ == '__main__':
+
+    # Verify implementations
+    bart1 = User(1, 'bart', 'EatMyShorts!')
+    bart2 = User(1, 'bart', '#Kig%/5gT54$§')
+    assert bart1 == bart2
+    assert "User: id=1, username='bart'" == str(bart1)
+
+    homer = User(3, 'homer', 'MoreDuff4Me!')
+    marge = User(7, 'marge', 'Kh7gT.9/8#gH')
+
+    table = UserTable()
+    table.insert(homer)
+    table.insert(marge)
+
+    marge_user = table.find_by_id(7)
+    assert marge_user is not None           # find_by_id(self, oid:int) -> None
+    assert 'marge' == marge_user.username   # find_by_id(self, oid:int) -> User
+
+    homer_user = table.find_by_id(3)
+    assert homer_user is not None           # find_by_id(self, oid:int) -> None
+    assert 'homer' == homer_user.username   # find_by_id(self, oid:int) -> User
+    assert 2 == len(table.find_all())
