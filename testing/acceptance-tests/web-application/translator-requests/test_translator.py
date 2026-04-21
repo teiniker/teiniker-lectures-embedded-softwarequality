8a860d010ef479f513a6649e68aae5c6c12c1996
teiniker <egon.teiniker@gmail.com>
2026-04-21 16:23:09 +0200

    Refactoring to PyTest.

---
+++ testing/acceptance-tests/web-application/translator-requests/test_translator.py
@@ -0,0 +1,26 @@
+import requests
+
+BASE_URL = 'http://localhost:8080'
+
+
+def test_get_request():
+    # Exercise
+    response = requests.get(f'{BASE_URL}/', timeout=5)
+    # Verify
+    assert response.status_code == 200
+
+
+def test_post_request():
+    # Exercise
+    data = {'word': 'cat', 'language': 'Deutsch', 'action': 'Translate'}
+    response = requests.post(f'{BASE_URL}/translator', data, timeout=5)
+    # Verify  
+    assert response.status_code == 200
+    assert 'Translate: cat into Katze' in response.text
+
+
+def test_get_request_headers():
+    # Exercise
+    response = requests.get(f'{BASE_URL}/', timeout=5)
+    # Verify
+    assert response.headers['Content-Type'] == 'text/html;charset=UTF-8'
