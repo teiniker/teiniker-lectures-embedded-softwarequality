df4721b607feccfba58bba7cfb11ed0c2fd1aa66
teiniker <egon.teiniker@gmail.com>
2026-05-12 14:45:17 +0200

    Refactoring.

--- testing/acceptance-tests/web-application/translator_selenium/test_translator.py
+++ testing/acceptance-tests/web-application/translator_selenium/test_translator.py
@@ -12,20 +12,30 @@ class TestTranslator:
 
     def test_cat_german(self):
         self.driver.get("http://localhost:8080")
+        # Enter word
         self.driver.find_element(By.NAME, "word").click()
         self.driver.find_element(By.NAME, "word").send_keys("cat")
+        # Select language
         self.driver.find_element(By.NAME, "language").click()
         self.driver.find_element(By.CSS_SELECTOR, "option:nth-child(1)").click()
+        # Click translate
         self.driver.find_element(By.CSS_SELECTOR, "th:nth-child(3) > input").click()
+        # Assert text
         assert self.driver.find_element(By.CSS_SELECTOR, "p:nth-child(1)").text == "Translate: cat into Katze"
+        # Click back link
         self.driver.find_element(By.LINK_TEXT, "back").click()
 
     def test_cat_french(self):
         self.driver.get("http://localhost:8080")
+        # Enter word
         self.driver.find_element(By.NAME, "word").click()
         self.driver.find_element(By.NAME, "word").send_keys("cat")
+        # Select language
         self.driver.find_element(By.NAME, "language").click()
         self.driver.find_element(By.CSS_SELECTOR, "option:nth-child(2)").click()
+        # Click translate
         self.driver.find_element(By.CSS_SELECTOR, "th:nth-child(3) > input").click()
+        # Assert text
         assert self.driver.find_element(By.CSS_SELECTOR, "p:nth-child(1)").text == "Translate: cat into Chatte"
+        # Click back link
         self.driver.find_element(By.LINK_TEXT, "back").click()
