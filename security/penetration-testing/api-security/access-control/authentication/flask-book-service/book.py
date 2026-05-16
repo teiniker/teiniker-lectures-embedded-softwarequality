f68552a3a2a2646045ebf850187b74579fc742d9
teiniker <egon.teiniker@gmail.com>
2026-05-16 13:16:19 +0200

    Moved pen testing and risk analysis to CI/CD repo.

--- security/penetration-testing/api-security/access-control/authentication/flask-book-service/book.py
+++
@@ -1,8 +0,0 @@
-
-class BookDTO:
-    """Transfer object for Book data"""
-    def __init__(self, oid:int, author:str, isbn:str) -> None:
-        self.oid = oid
-        self.author = author
-        self.isbn = isbn
-
