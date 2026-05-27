111214f8c1dc120a299dd3fa343c01032d80846b
teiniker <egon.teiniker@gmail.com>
2026-05-27 15:04:55 +0200

    Refactoring.

--- security/cryptography/key_derivation_function/hashlib_scrypt.py
+++ security/cryptography/key_derivation_function/hashlib_scrypt.py
@@ -7,7 +7,7 @@ class ScryptTest(unittest.TestCase):
     def test_scrypt(self):
         salt = secrets.token_bytes(16)
         password = b'password'
-        key_len = 32        
+        key_len = 32
         key = scrypt(password, salt=salt, n=2**14, r=8, p=1, dklen=key_len)
         print(key.hex())
 
